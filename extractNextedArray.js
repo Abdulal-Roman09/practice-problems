25830a6d92dd08576a5cef6fc02e3218ead9aa7f
Abdul Al Roman <mdroman45678910@gmail.com>
2025-08-25 21:21:21 +0000

    flate arr

--- extractNextedArray.js
+++ extractNextedArray.js
@@ -1,11 +1,21 @@
-function extractNestedArray(arr) {
+// function extract(arr){
+//     const flatArr=arr.flat(Infinity)
+//     return flatArr;
+// }
+// console.log(extract([1,2,[3,4,[5,6]]]));
+
+
+function flatten(arr) {
     let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            result = result.concat(extractNestedArray(arr[i]));
+    for (let i of arr) {
+        if (Array.isArray(i)) {
+            result = result.concat(flatten(i)); // recursion
         } else {
-            result.push(arr[i]);
+            result.push(i);
         }
     }
     return result;
 }
+
+const arr = [1, [2, [3, 4]], 5];
+console.log(flatten(arr)); // [1, 2, 3, 4, 5]
