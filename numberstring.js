7fa34d1359ddebbf736feaa5eaf7e2e6f1d3b39a
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-17 06:45:48 +0000

    some example of nubmer and string relation

---
+++ numberstring.js
@@ -0,0 +1,9 @@
+// example of number to string conversion
+// console.log(typeof(1234+"22"))
+// console.log(typeof(("22"+1234)))
+// console.log(typeof(("22"+"22")))
+
+console.log(1234+"22")
+console.log("22"+1234+33)
+console.log(1234+33+"22")
+console.log("22"+"22")
