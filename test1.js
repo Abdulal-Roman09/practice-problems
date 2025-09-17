25df3d5dabd91ff094f71897e029526266849ed2
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-17 06:23:35 +0000

    test1.js

---
+++ test1.js
@@ -0,0 +1,14 @@
+// example of array in different ways
+// let arr=[,,,]
+// console.log(arr.length)
+// let arr2=[1,,3]
+// console.log(arr2.length)
+// let arr3=[[1,2,3],[4,5,6]]
+// console.log(arr3.length)
+
+// example of map function
+let arr4=[1,2,3]                    
+arr4.map((singleitems)=>{
+    console.log(singleitems*2>3)
+})
+
